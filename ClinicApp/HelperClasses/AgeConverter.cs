1d6b53911cca7ea21206c27d73bf3b453b1778ef
Uladzimirant <vladimir.bwmail@gmail.com>
2023-09-21 14:02:59 +0300

    +Added age column

---
+++ ClinicApp/HelperClasses/AgeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ClinicApp.XamlPages
+{
+    public class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime birthDate = (DateTime)value;
+            int yearDifference = DateTime.Now.Year - birthDate.Year;
+            return yearDifference + (birthDate.AddYears(yearDifference) >= DateTime.Now ? 1 : 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
